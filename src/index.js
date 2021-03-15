0ec374e0437050c1ad8e8b5992706e8a174763ff
Eliz39 <liza.nikitina9898@gmail.com>
2021-03-15 21:03:28 +0100

    feat: change src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array === undefined || array.length === 0) { return 0}
+    else {
+      var min = array[0];
+      for(var i = 0; i < array.length; i++) {
+        if(array[i] <= min) min = array[i];
+    }
+    return min;
+};0 }
+
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) { return 0}
+    else{
+      var max = array[0];
+    for(var i = 0; i < array.length; i++) {
+        if(array[i] >= max) max = array[i];
+       } return max;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    var average = 0;
+    if (array === undefined || array.length === 0) { return 0}
+    else {
+      for(var i = 0; i < array.length; i++) {
+        average += array[i];
+}
+        average /= array.length;
+  return average;
+    }
 }
